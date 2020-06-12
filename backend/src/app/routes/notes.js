a663ca709744b7c554995d3023c499a4bd2c5685
Gabriely Rodrigues <gabriely.bleez@gmail.com>
2020-06-11 21:53:32 -0300

    exibindo nota (se ela pertencer ao usuário da request)

--- backend/src/app/routes/notes.js
+++ backend/src/app/routes/notes.js
@@ -4,6 +4,7 @@ let router = express.Router();
 const Note = require('../models/note');
 const withAuth = require('../middlewares/auth');
 
+//cadastrar nota
 router.post('/', withAuth, async (request, response) => {
     const { title, body } = request.body;
 
@@ -16,5 +17,32 @@ router.post('/', withAuth, async (request, response) => {
         return response.status(500).json({ error: "Error saving note. Try again." });
     }
 });
+
+//exibir nota
+router.get('/:id', withAuth, async (request, response) => {
+    const { id } = request.params;
+
+    try {
+        let note = await Note.findById(id);
+
+        if(isOwner(request.user, note)) {
+            return response.status(200).json(note);
+        } else {
+            return response.status(403).json({ error: "Permission denied. You are not the note owner."});
+        }
+        
+    } catch (error) {
+        return response.status(500).json({ error: "Error finding note. Try again." });
+    }
+});
+
+//verificar se o usuário é o dono da nota
+const isOwner = (user, note) => {
+    if(JSON.stringify(user._id) == JSON.stringify(note.author._id)) {
+        return true;
+    } else {
+        return false;
+    }
+}
 
 module.exports = router;
